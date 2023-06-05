9d3d3fe3d0384ebfa89cd9fd85d65d2c66f51a35
numangit <abdulnumanjob@gmail.com>
2023-06-05 19:07:48 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5001;
 const app = express();
@@ -7,6 +8,21 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//connection with mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zbie1as.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    //database collections
+    const tasksCollection = client.db('MCS-task-manager').collection('tasks');
+  }
+  finally {
+  }
+}
+
+run().catch(console.log)
 
 app.get('/', async (req, res) => {
   res.send('Server is running')
